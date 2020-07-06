f1c5595e4cc1db4329e564f39a83563295202218
wangjian <wangjian890523@gmail.com>
2020-07-07 00:36:11 +0800

    修改解析器解析网页数据不完整问题

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -3,7 +3,6 @@ package fetcher
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -29,13 +28,14 @@ func Fetch(url string) ([]byte, error) {
 			fmt.Errorf("wrong status code:%d", resp.StatusCode)
 	}
 	// 如果是gbk 编码转换成utf8
-	e := determineEncoding(resp.Body)
-	utf8reader := transform.NewReader(resp.Body, e.NewDecoder())
+	bodyReader := bufio.NewReader(resp.Body)
+	e := determineEncoding(bodyReader)
+	utf8reader := transform.NewReader(bodyReader, e.NewDecoder())
 	return ioutil.ReadAll(utf8reader)
 }
 
-func determineEncoding(r io.Reader) encoding.Encoding {
-	bytes, err := bufio.NewReader(r).Peek(1024)
+func determineEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 	if err != nil {
 		//panic(err)
 		log.Printf("Fetch error:%d", err)
