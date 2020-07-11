1ec9d7264a0263e3abf174ce67e048b0a4ade859
wangjian <wangjian890523@gmail.com>
2020-07-12 01:14:37 +0800

    整理代码，解决多用户多次调用问题

--- zhenai/parser/profile.go
+++ zhenai/parser/profile.go
@@ -5,29 +5,27 @@ import (
 	"strconv"
 )
 
-const ageRe = `<td><span class="label">年龄:</span>([\d]+)岁</td>`
-const marriageRe = `<td><span class="label">婚况:</span>([^<]+)岁</td>`
+var ageRe = regexp.MustCompile(`<td><span class="label">年龄:</span>([\d]+)岁</td>`)
+var marriageRe = regexpp.MustCompile(`<td><span class="label">婚况:</span>([^<]+)岁</td>`)
 
 func ParseProfile(contents []byte) engine.PareseResult {
 	profile := modle.Profile{}
-	re := regexp.MustCompile(ageRe)
-	match := re.FindSubmatch(contents)
 
-	if match != nil {
-		age, err := strconv.Atoi(string(match[1]))
-		if err != nil {
-			profile.Age = age
-		}
-	}
+	age, err := strconv.Atoi(extractString(contents, ageRe))
+	if err != nil {
+		profile.Age = age
 
-	re = regexp.MustCompile(marriageRe)
-	match = re.FindSubmatch(contents)
-	if match != nil {
-		profile.Marriage = string(match[1])
 	}
+	profile.Marriage = extractString(contents, marriageRe)
 
 }
 
-func extractString(contents []byte, re string) {
+func extractString(contents []byte, re *regexp.Regexp) string {
+	match = re.FindSubmatch(contents)
+	if len(match) >= 2 {
+		return string(match[1])
+	} else {
+		return ""
+	}
 
 }
